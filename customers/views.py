cffd6ee35812c603979d18e3e3a3242c54b5328e
Aby <stinkweed8137@gmail.com>
2025-02-06 19:21:00 +0530

    Customer Login Registration updated

--- customers/views.py
+++ customers/views.py
@@ -1,6 +1,71 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from .models import Customer
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
 
-# Create your views here.
+def sign_out(request):
+    logout(request)
+    return redirect('home')
 
 def show_account(request):
+    if request.POST and 'register' in request.POST: #if post request and register in request
+
+        try:
+            username=request.POST.get('username')
+            email=request.POST.get('email')
+            password=request.POST.get('password')
+            address=request.POST.get('address')
+            phoneno=request.POST.get('phoneno')
+
+            #create user object
+            user=User.objects.create(
+                username=username,
+                email=email
+            )
+            user.set_password(password)  # Hash the password before saving
+            user.save()
+
+            '''or
+            #create user object using create_user
+            user=User.objects.create_user(
+                username=username,
+                email=email,
+                password=password
+            )
+            '''
+
+            #create customer object using user object
+            customer=Customer.objects.create(
+                user=user,
+                address=address,
+                phone=phoneno
+            )
+            sucess_mesage = "User Registred Sucessfully"
+            messages.success(request, sucess_mesage)
+        
+        except:
+            error_message="Duplicate username or Invalid Credintials"
+            messages.error(request, error_message)
+    
+    elif request.POST and 'login' in request.POST:
+        try:
+            username=request.POST.get('username')
+            password=request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:  # If user is found and credentials are correct
+                login(request, user)
+                success_message = "Login successful"
+                messages.success(request, success_message)
+            
+            else:
+                error_message = "Invalid credentials"
+                messages.error(request, error_message)
+        
+        except Exception as e:
+            error_message = "An error occurred during login: " + str(e)
+            messages.error(request, error_message)
+
     return render(request, 'account.html')
--- products/urls.py
+++ products/urls.py
@@ -6,7 +6,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='home'),
     path('product_list/', views.list_products, name='list_product'),
     path('product_detail/<pk>/', views.detail_product, name='detail_product'),
 ]
